7c4251211bd89f9d4649ad9bf1b7b58a14098f6e
Sofiia Tyshchenko <styshchenko@kse.org.ua>
2023-03-10 13:20:36 +0200

    first commit

---
+++ doodle-maps/Program.cs
@@ -0,0 +1,25 @@
+﻿using Kse.Algorithms.Samples;
+
+var generator = new MapGenerator(new MapGeneratorOptions()
+{
+    Height = 35,
+    Width = 90,
+});
+
+string[,] map = generator.Generate();
+new MapPrinter().Print(map);
+
+
+List<Point> GetShortestPath(string[,] map, Point start, Point goal)
+{
+    // your code here
+}
+
+
+// declare
+var distances = new Dictionary<Point, int>();
+var origins = new Dictionary<Point, Point>();
+  
+// and use
+distances[point] = distance;
+origins[point] = origin;
